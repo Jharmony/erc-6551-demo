a82d193e52623988b427e0aa375df5c3813ca5db
bibitto <bibitto.crypto@gmail.com>
2023-05-26 21:29:39 +0900

    deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,24 +1,32 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+    const TestNFT = await ethers.getContractFactory("TestNFT");
+    const testNft = await TestNFT.deploy();
+    // TestToken
+    const TestToken = await ethers.getContractFactory("TestToken");
+    const testToken = await TestToken.deploy();
+    // ERC6551Registry
+    const ERC6551Registry = await ethers.getContractFactory("ERC6551Registry");
+    const registry = await ERC6551Registry.deploy();
+    // ERC6551Account
+    const ERC6551Account = await ethers.getContractFactory("ERC6551Account");
+    const account = await ERC6551Account.deploy();
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+    await testNft.deployed();
+    await testToken.deployed();
+    await registry.deployed();
+    await account.deployed();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+    console.log("testNft -> ", testNft.address);
+    console.log("testToken -> ", testToken.address);
+    console.log("registry -> ", registry.address);
+    console.log("account -> ", account.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
