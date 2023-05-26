4d6ec8f74a1d1f2c26b1441d3efa0bb6fdeabd4c
bibitto <bibitto.crypto@gmail.com>
2023-05-26 21:30:35 +0900

    setup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,26 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
+require("dotenv").config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+    solidity: "0.8.13",
+    etherscan: {
+        apiKey: {
+            avalancheFujiTestnet: `${process.env.AVALANCHE_API_KEY}`,
+            polygonMumbai: `${process.env.MUMBAI_API_KEY}`,
+        },
+    },
+    networks: {
+        avalancheFujiTestnet: {
+            url: `${process.env.FUJI_RPC_URL}`,
+            accounts: [`${process.env.PRIVATE_KEY}`],
+        },
+        polygonMumbai: {
+            url: `${process.env.MUMBAI_RPC_URL}`,
+            accounts: [`${process.env.PRIVATE_KEY}`],
+        },
+    },
 };
 
 export default config;
